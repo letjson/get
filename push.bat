7b34cba41f162bee2be2ead6d77ff74667baf96f
tom <tom@sapletta.com>
2020-06-08 13:18:25 +0200

    first version

--- push.bat
+++ push.bat
@@ -5,4 +5,4 @@ SetLocal EnableDelayedExpansion
 for /f %%i in ('dir /b /od Ticket\*.md') do set filename=%%i
 for /f "delims==" %%a in (Ticket\%filename%) do set message=%%a
 @ECHO ON
-git add . && git commit -m "%message%" && git push origin master && start firefox "https://github.com/js-func/each.git"
+build.bat && git add . && git commit -m "%message%" && git push origin master && docs.bat

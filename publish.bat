6466ecb9b90eb3d3c653f9371d12337587ca7c74
tom <tom@sapletta.com>
2020-06-08 13:28:13 +0200

    publish

--- publish.bat
+++ publish.bat
@@ -2,4 +2,4 @@
 set /P VERSION= < .apicra\variable\VERSION.txt
 git push origin v%VERSION%
 ::npm publish --access public && @ping -n 4 localhost> nul && echo "Optionally put Your defined Version: X.Y.Z" && -version.bat && -ticket.bat && -open-npm-url.bat
-github-push.bat && version.bat && github-push.bat && npm-publish.bat
+::github-push.bat && version.bat && github-push.bat && npm-publish.bat

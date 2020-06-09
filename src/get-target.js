724328c752324e92d06d1120d5504ba6c49c29c0
tom <tom@sapletta.com>
2020-06-09 11:05:56 +0200

    better version

--- src/get-target.js
+++ src/get-target.js
@@ -9,13 +9,14 @@ if (typeof log !== 'function') {
  * @returns {HTMLHeadElement}
  */
 function getTarget(target) {
-    log(this.constructor.name, ' target ', target);
+    this.constructor.name = 'getTarget';
+    // log(this.constructor.name, ' target ', target);
     if (isEmpty(target)) {
         target = document.getElementsByTagName('head')[0];
-        log(this.constructor.name, ' HEAD ', target, typeof target, target.innerHTML !== 'undefined',  target.innerHTML.length, Object.keys(target));
+        log(this.constructor.name, ' isEmpty HEAD ', target, typeof target, target.innerHTML !== 'undefined',  target.innerHTML.length, Object.keys(target));
         if (isEmpty(target)) {
             target = document.body;
-            log(this.constructor.name, ' BODY ', target);
+            log(this.constructor.name, ' isEmpty BODY ', target);
         }
     }
     log(this.constructor.name, ' target ', target);

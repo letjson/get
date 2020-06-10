fccf6d258a775aec937f55ad820f47b4bd9dd407
tom <tom@sapletta.com>
2020-06-10 16:46:16 +0200

    better version

--- src/load-all.js
+++ src/load-all.js
@@ -121,7 +121,7 @@ function getOne(jloads, object, i, mapFunction, success, error) {
     elem = document.querySelectorAll(i)[0] || document.querySelectorAll(i) || document.body;
     console.log('loadAll getOne ', ' elem ', elem, !isEmpty(elem));
 
-    if (i !== 'head' && !isEmpty(elem)) {
+    if (i !== 'head' && elem) {
         console.log('loadAll getOne ', ' !isEmpty ', elem, !isEmpty(elem));
         success(elem);
         loadContentByUrls(jloads, object, elem, mapFunction, success, error);

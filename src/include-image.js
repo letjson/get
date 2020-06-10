dc2ca5566d0c627f113657d3d68c8fd0a84999c9
tom <tom@sapletta.com>
2020-06-10 20:16:00 +0200

    better version

--- src/include-image.js
+++ src/include-image.js
@@ -12,24 +12,24 @@ if (typeof log !== 'function') {
  * @returns {boolean|*}
  */
 const includeImage = function (url, target, replace, success, error) {
-
-    log(this.constructor.name, ' includeImg url: ', url);
+    const f = 'includeImage';
+    log(f, ' includeImg url: ', url);
     // JLOADS_DEBUG || log('el', el);
 
     let img = new Image;
     img.onload = function () {
-        log(this.constructor.name, "include Image onload url: ", url);
-        log(this.constructor.name, "include Image replace: ", replace);
+        log(f, "include Image onload url: ", url);
+        log(f, "include Image replace: ", replace);
 
         if (typeof replace === 'number' && replace === 1) {
             replace = true;
         }
         // JLOADS_DEBUG || log('typeof self.cfg.replace', typeof self.cfg.replace);
-        log(this.constructor.name, "include Image replace: ", replace);
+        log(f, "include Image replace: ", replace);
 
 
         if (replace) {
-            log(this.constructor.name, 'includeImage elmnt firstChild: ', elmnt.firstChild);
+            log(f, 'includeImage elmnt firstChild: ', elmnt.firstChild);
             elmnt.removeChild(elmnt.firstChild);
             // let element = document.getElementById("top");
             // while (element.firstChild) {

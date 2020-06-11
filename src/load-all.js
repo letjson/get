b90679ee405da5a6a1f4ee1f2de1519103710a8c
tom <tom@sapletta.com>
2020-06-11 22:13:24 +0200

    jlogs

--- src/load-all.js
+++ src/load-all.js
@@ -17,7 +17,8 @@ var mapFunction = {
     'html5': 'html'
 }
 
-jlogs('exist?','getFileExtension');
+jlogs('exist?', 'getFileExtension');
+
 /**
  *
  * @param filename
@@ -27,7 +28,8 @@ function getFileExtension(filename) {
     return filename.split("?")[0].split("#")[0].split('.').pop();
 }
 
-jlogs('exist?','getFunctionName');
+jlogs('exist?', 'getFunctionName');
+
 /**
  *
  * @param url
@@ -38,8 +40,8 @@ function getFunctionName(url, map) {
     const f = 'getFunctionName';
 
     var ext = getFileExtension(url)
-   jlogs(f, ' url ', url);
-   jlogs(f, ' map ', map);
+    jlogs(f, ' url ', url);
+    jlogs(f, ' map ', map);
     var result = map[ext];
 
     if (isEmpty(result)) {
@@ -48,7 +50,8 @@ function getFunctionName(url, map) {
     return result;
 }
 
-jlogs('exist?','loadAll');
+jlogs('exist?', 'loadAll');
+
 /**
  *
  * @param json
@@ -67,10 +70,10 @@ function loadAll(json, success, error, mapFunction) {
     if (typeof success !== 'function' && (typeof success !== 'object' || success === null)) {
         // Configuration
         success = function (data) {
-           jlogs('loadAll loaded ', data);
+            console.log('loadAll loaded ', data);
         };
         error = function (data) {
-            err('loadAll !loaded ', data);
+            console.error('loadAll !loaded ', data);
         };
     }
 
@@ -90,11 +93,11 @@ function loadAll(json, success, error, mapFunction) {
             'html5': 'html'
         }
     }
-   jlogs(' loadAll', ' json ', json, Object.keys(json).length, Object.keys(json)[0]);
+    jlogs(' loadAll', ' json ', json, Object.keys(json).length, Object.keys(json)[0]);
 
 
     var elem = document.querySelectorAll(i)[0] || document.querySelectorAll(i) || document.body;
-   jlogs('loadAll getOne ', ' elem ', elem, !isEmpty(elem));
+    jlogs('loadAll getOne ', ' elem ', elem, !isEmpty(elem));
 
     var jloads = new Load(elem, success, error);
 
@@ -112,7 +115,8 @@ function loadAll(json, success, error, mapFunction) {
     return jloads;
 }
 
-jlogs('exist?','getOne');
+jlogs('exist?', 'getOne');
+
 /**
  *
  * @param jloads
@@ -125,23 +129,23 @@ jlogs('exist?','getOne');
 function getOne(jloads, object, i, mapFunction, success, error) {
     const f = 'loadAll getOne';
 
-   jlogs(f, ' jloads.getTarget() ', jloads.getTarget());
+    jlogs(f, ' jloads.getTarget() ', jloads.getTarget());
 
     // TODO: move to class E for smart load content on not existing DOM elements
     // if (i === 'head' || !isEmpty(jloads.getTarget())) {
-   jlogs(f, ' object i ', object, i);
+    jlogs(f, ' object i ', object, i);
     if (i === 'head') {
         loadContentByUrls(jloads, object, mapFunction, success, error);
         success(jloads.getTarget());
     } else if (i === 'body') {
-       jlogs(f, ' wait for body i ', i);
-       jlogs(f, ' wait for body target ', jloads.getTarget());
+        jlogs(f, ' wait for body i ', i);
+        jlogs(f, ' wait for body target ', jloads.getTarget());
         document.addEventListener("DOMContentLoaded", function () {
             ReadyHtml(object, i, mapFunction, success, error);
         });
     } else {
-       jlogs(f, ' wait for element i ', i);
-       jlogs(f, ' wait for element target ', jloads.getTarget());
+        jlogs(f, ' wait for element i ', i);
+        jlogs(f, ' wait for element target ', jloads.getTarget());
 
         try {
             // set up the mutation observer
@@ -166,14 +170,15 @@ function getOne(jloads, object, i, mapFunction, success, error) {
 
         } catch (e) {
             //jlogs(f, ' ERROR elem ', elem);
-           jlogs(f, ' getOne ERROR e ', e);
+            jlogs(f, ' getOne ERROR e ', e);
             error(e);
         }
     }
     // error(elem);
 }
 
-jlogs('exist?','loadContentByUrls');
+jlogs('exist?', 'loadContentByUrls');
+
 /**
  *
  * @param jloads
@@ -186,25 +191,25 @@ function loadContentByUrls(jloads, object, mapFunction, success, error) {
 
     const f = 'loadAll loadContentByUrls';
 
-   jlogs(f, ' isArray object, elem, mapFunction', object, isArray(object), mapFunction);
+    jlogs(f, ' isArray object, elem, mapFunction', object, isArray(object), mapFunction);
 
     if (isArray(object)) {
         var url = '';
         for (var id in object) {
-           jlogs(f, ' isArray', ' id ', id);
+            jlogs(f, ' isArray', ' id ', id);
             url = object[id];
-           jlogs(f, ' isArray', ' url ', url);
+            jlogs(f, ' isArray', ' url ', url);
 
             if (typeof url === 'string') {
                 try {
                     const funcName = getFunctionName(url, mapFunction);
-                   jlogs(f, ' funcName ', funcName);
+                    jlogs(f, ' funcName ', funcName);
                     //jlogs(funcName, url, elem);
                     jloads[funcName](url);
                     success(url);
                 } catch (e) {
                     //jlogs(f, ' ERROR elem ', elem);
-                   jlogs(f, ' ERROR e ', e);
+                    jlogs(f, ' ERROR e ', e);
                     error(e);
                 }
 
@@ -213,12 +218,13 @@ function loadContentByUrls(jloads, object, mapFunction, success, error) {
             }
         }
     } else {
-       jlogs(f, ' isArray ERROR object', object);
+        jlogs(f, ' isArray ERROR object', object);
         error(object);
     }
 }
 
-jlogs('exist?','ReadyHtml');
+jlogs('exist?', 'ReadyHtml');
+
 /**
  *
  * @param object
@@ -232,9 +238,9 @@ jlogs('exist?','ReadyHtml');
 function ReadyHtml(object, i, mapFunction, success, error) {
     const f = 'loadAll ReadyHtml';
 
-   jlogs(f, ' i ', i);
+    jlogs(f, ' i ', i);
     var elem = document.querySelectorAll(i)[0] || document.querySelectorAll(i) || document.body;
-   jlogs(f, ' elem ', elem);
+    jlogs(f, ' elem ', elem);
 
     var jloads = new Load(elem, success, error);
 
@@ -242,7 +248,7 @@ function ReadyHtml(object, i, mapFunction, success, error) {
         loadContentByUrls(jloads, object, mapFunction, success, error);
         success(elem);
     } else {
-        waitForElementToDisplay(i, 200, function (i){
+        waitForElementToDisplay(i, 200, function (i) {
             var elem = document.querySelectorAll(i)[0] || document.querySelectorAll(i);
             var jloads = new Load(elem, success, error);
             loadContentByUrls(jloads, object, mapFunction, success, error);
@@ -251,7 +257,8 @@ function ReadyHtml(object, i, mapFunction, success, error) {
     }
 }
 
-jlogs('exist?','waitForElementToDisplay');
+jlogs('exist?', 'waitForElementToDisplay');
+
 /**
  *
  * @param selector
@@ -261,7 +268,7 @@ jlogs('exist?','waitForElementToDisplay');
  */
 function waitForElementToDisplay(selector, time, callback) {
     const f = 'waitForElementToDisplay';
-   jlogs(f, ' selector ', selector);
+    jlogs(f, ' selector ', selector);
     if (document.querySelector(selector) != null) {
         // alert("The element is displayed, you can put your code instead of this alert.")
         return callback(selector);

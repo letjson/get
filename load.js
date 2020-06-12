3c1cf3ceff333141d899e136ba9de25ab16182f2
tom <tom@sapletta.com>
2020-06-12 15:42:16 +0200

    jlogs

--- load.js
+++ load.js
@@ -658,29 +658,33 @@ const includeImage = function (url, target, replace, success, error) {
     jlogs(f, ' includeImg url: ', url);
     jlogs(f, ' includeImg target: ', target);
 
+
     let img = new Image;
-    img.onload = function () {
-        jlogs(f, "include Image onload url: ", url);
-        jlogs(f, "include Image replace: ", replace);
+    // if base64
+    if (url.length < 200) {
+        img.onload = function () {
+            jlogs(f, "include Image onload url: ", url);
+            // jlogs(f, "include Image replace: ", replace);
 
-        if (typeof replace === 'number' && replace === 1) {
-            replace = true;
-        }
-        // JLOADS_DEBUG ||jlogs('typeof self.cfg.replace', typeof self.cfg.replace);
-        jlogs(f, "include Image replace: ", replace);
+            if (typeof replace === 'number' && replace === 1) {
+                replace = true;
+            }
+            // JLOADS_DEBUG ||jlogs('typeof self.cfg.replace', typeof self.cfg.replace);
+            jlogs(f, "include Image replace: ", replace);
 
 
-        if (replace) {
-            jlogs(f, 'includeImage getTarget(target): ', getTarget(target));
-            jlogs(f, 'includeImage getTarget(target) firstChild: ', getTarget(target).firstChild);
-            getTarget(target).removeChild(getTarget(target).firstChild);
-            // let element = document.getElementById("top");
-            // while (element.firstChild) {
-            //     element.removeChild(element.firstChild);
-            // }
-        }
-        getTarget(target).appendChild(img);
-    };
+            if (replace) {
+                jlogs(f, 'includeImage getTarget(target): ', getTarget(target));
+                jlogs(f, 'includeImage getTarget(target) firstChild: ', getTarget(target).firstChild);
+                getTarget(target).removeChild(getTarget(target).firstChild);
+                // let element = document.getElementById("top");
+                // while (element.firstChild) {
+                //     element.removeChild(element.firstChild);
+                // }
+            }
+            getTarget(target).appendChild(img);
+        };
+    }
 
     return img.src = url;  // erst nach dem Event Listener!
 }

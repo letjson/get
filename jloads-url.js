316e93735f299b12521475ea0100e15e05a6ee3d
tom <tom@sapletta.com>
2020-06-13 16:59:44 +0200

    rename to jloads-url.js

--- jloads-url.js
+++ jloads-url.js
@@ -111,19 +111,22 @@ jlogs('exist?', 'getTarget');
 function getTarget(selector) {
     const f = 'getTarget';
 
-    var target = document.querySelectorAll(selector)[0] || document.querySelectorAll(selector); // || document.body;
-    jlogs(f, 'selector', selector, target);
-
-    //jlogs(f, ' target ', target);
-    if (isEmpty(target)) {
-        target = document.getElementsByTagName('head')[0];
-        jlogs(f, ' isEmpty HEAD ', target, typeof target, target.innerHTML !== 'undefined', target.innerHTML.length, Object.keys(target));
-        if (isEmpty(target)) {
-            target = document.body;
-            jlogs(f, ' isEmpty BODY ', target);
-        }
+    if(selector === 'string'){
+        jlogs(f, 'selector', selector);
+        var target = document.querySelectorAll(selector)[0] || document.querySelectorAll(selector) || document.getElementsByTagName('head')[0] || document.body;
     }
-    jlogs(f, ' target: ', target);
+
+    jlogs(f, 'target', target);
+    //jlogs(f, ' target ', target);
+    // if (isEmpty(target)) {
+    //     target = document.getElementsByTagName('head')[0];
+    //     jlogs(f, ' isEmpty HEAD ', target, typeof target, target.innerHTML !== 'undefined', target.innerHTML.length, Object.keys(target));
+    //     if (isEmpty(target)) {
+    //         target = document.body;
+    //         jlogs(f, ' isEmpty BODY ', target);
+    //     }
+    // }
+    // jlogs(f, 'target', target);
 
     return target;
 }

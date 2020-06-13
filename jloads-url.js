9d73fb36987c71e0a3bbc4c4e3af71ae6f420be2
tom <tom@sapletta.com>
2020-06-13 16:54:09 +0200

    rename to jloads-url.js

--- jloads-url.js
+++ jloads-url.js
@@ -101,26 +101,28 @@ var time = Date.now || function () {
     return +new Date;
 };
 // get-target.js
-jlogs('exist?','getTarget');
+jlogs('exist?', 'getTarget');
 
 /**
  *
  * @param target
  * @returns {HTMLHeadElement}
  */
-function getTarget(target) {
+function getTarget(selector) {
     const f = 'getTarget';
+
+    var target = document.querySelectorAll(selector)[0] || document.querySelectorAll(selector); // || document.body;
 
     //jlogs(f, ' target ', target);
     if (isEmpty(target)) {
         target = document.getElementsByTagName('head')[0];
-       jlogs(f, ' isEmpty HEAD ', target, typeof target, target.innerHTML !== 'undefined',  target.innerHTML.length, Object.keys(target));
+        jlogs(f, ' isEmpty HEAD ', target, typeof target, target.innerHTML !== 'undefined', target.innerHTML.length, Object.keys(target));
         if (isEmpty(target)) {
             target = document.body;
-           jlogs(f, ' isEmpty BODY ', target);
+            jlogs(f, ' isEmpty BODY ', target);
         }
     }
-   jlogs(f, ' target: ', target);
+    jlogs(f, ' target: ', target);
 
     return target;
 }

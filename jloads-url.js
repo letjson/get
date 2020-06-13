16d8eef41250f00a2402ada133097feb15832f61
tom <tom@sapletta.com>
2020-06-13 23:32:05 +0200

    rename to jloads-url.js

--- jloads-url.js
+++ jloads-url.js
@@ -907,8 +907,9 @@ jlogs('exist?', 'waitFor');
 function waitFor(selector, time, callback) {
     const f = 'waitFor';
     jlogs(f, ' selector ', selector);
-    jlogs(f, ' selector typeof ', typeof document.querySelector(selector));
-    if (typeof document.querySelector(selector) !== "null") {
+    console.log(f, ' selector document.querySelector(selector) ', typeof document.querySelector(selector), document.querySelector(selector));
+    console.log(f, ' selector document.querySelectorAll(selector) ', typeof document.querySelectorAll(selector), document.querySelectorAll(selector), document.querySelectorAll(selector).length);
+    if (typeof document.querySelectorAll(selector) === 'object' && document.querySelectorAll(selector).length > 0) {
         // alert("The element is displayed, you can put your code instead of this alert.")
         return callback(selector);
     } else {

cec15161edf9a63547c2f2b65252d30b538dcafd
tom <tom@sapletta.com>
2020-06-22 11:44:01 +0200

    javascript function routing, checking if exist and delay request or DOMContentLoaded event

---
+++ src/lib/each.js
@@ -0,0 +1,44 @@
+// each.js
+jlogs('exist?','each');
+
+/**
+ *
+ * @param array
+ * @param callback
+ * @param limit
+ * @param is_last
+ * @param is_first
+ */
+function each(array, callback, limit, is_last, is_first) {
+
+    // limit = 5;
+    var count = 1;
+    var obj = {};
+
+    if (is_first) {
+        obj = firstArray(array);
+        callback(obj);
+
+    } else if (is_last) {
+        obj = lastArray(array);
+        callback(obj);
+
+    } else {
+
+        for (var key in array) {
+
+            // skip loop if the property is from prototype
+            if (!array.hasOwnProperty(key)) continue;
+            if (count > limit) continue;
+            count++;
+
+            obj = array[key];
+
+            // console.log(obj);
+            // console.log(limit, count);
+
+            callback(obj);
+        }
+    }
+    // return this;
+}

9d95eb8b0e8690c2e285f3e51cff4b840e58fcef
tom <tom@sapletta.com>
2020-07-03 13:34:09 +0200

    javascript function routing, checking if exist and delay request or DOMContentLoaded event

---
+++ build-obj.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+type ".\src\meta\*.js" > .\jloads-obj.js
+type ".\src\core\*.js" >> .\jloads-obj.js
+type ".\src\lib\*.js" >> .\jloads-obj.js
+type ".\src\target\*.js" >> .\jloads-obj.js
+type ".\src\*.js" >> .\jloads-obj.js
+type ".\src\form\*.js" >> .\jloads-obj.js
+type ".\src\obj\*.js" >> .\jloads-obj.js

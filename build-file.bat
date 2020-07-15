3922487d5b05e89ec1beb70b387b49fed1e34d4a
tom <tom@sapletta.com>
2020-07-15 12:40:15 +0200

    New File solution for fast loading depends of files tree structure

--- build-file.bat
+++ build-file.bat
@@ -1,14 +1,14 @@
 @ECHO OFF
-type ".\src\meta\*.js" > .\jloads.js
-type ".\src\core\*.js" >> .\jloads.js
-type "..\apifunc-js\src\*.js" >> .\jloads.js
+type ".\src\meta\*.js" > .\jloads-file.js
+type ".\src\core\*.js" >> .\jloads-file.js
+type "..\apifunc-js\src\*.js" >> .\jloads-file.js
 :: load content remotely if not exist
-type ".\src\include\*.js" >> .\jloads.js
-type ".\src\load\*.js" >> .\jloads.js
-type ".\src\*.js" >> .\jloads.js
-type ".\src\file\*.js" >> .\jloads.js
-type ".\src\all\*.js" >> .\jloads.js
-::type ".\src\target\*.js" >> .\jloads.js
-::type ".\src\event\*.js" >> .\jloads.js
-::type ".\src\form\*.js" >> .\jloads.js
-::type ".\src\obj\*.js" >> .\jloads.js
+type ".\src\include\*.js" >> .\jloads-file.js
+type ".\src\load\*.js" >> .\jloads-file.js
+type ".\src\*.js" >> .\jloads-file.js
+type ".\src\all\*.js" >> .\jloads-file.js
+type ".\src\file\*.js" >> .\jloads-file.js
+::type ".\src\target\*.js" >> .\jloads-file.js
+::type ".\src\event\*.js" >> .\jloads-file.js
+::type ".\src\form\*.js" >> .\jloads-file.js
+::type ".\src\obj\*.js" >> .\jloads-file.js

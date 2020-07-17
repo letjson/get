bb29b0e60583cc8834dd62bcedff545d6aa6dd05
tom <tom@sapletta.com>
2020-07-17 14:50:34 +0200

    New File solution for fast loading depends of files tree structure

--- jloads-event.js
+++ jloads-event.js
@@ -1781,63 +1781,69 @@ if (typeof getOne !== 'function') getOne = function (load, url, selector, mapFun
         // if (selector === 'head') {
         //     loadContentByUrls(load, url, mapFunction, success, error);
         //     success(load.getTarget(selector));
-            // } else if (selector === 'body') {
-            //     jlogs(f, ' wait for body selector ', selector);
-            //     jlogs(f, ' wait for body target ', load.getTarget(selector));
+        // } else if (selector === 'body') {
+        //     jlogs(f, ' wait for body selector ', selector);
+        //     jlogs(f, ' wait for body target ', load.getTarget(selector));
 
-            // var reload = document.querySelector(selector);
-            // reload.addEventListener("load", function (event) {
-            //
-            //     console.log(f, 'event::', event);
-            //
-            //     ReadyHtml(url, selector, mapFunction, success, error);
-            // });
+        // var reload = document.querySelector(selector);
+        // reload.addEventListener("load", function (event) {
+        //
+        //     console.log(f, 'event::', event);
+        //
+        //     ReadyHtml(url, selector, mapFunction, success, error);
+        // });
         // } else {
-            jlogs(f, ' wait for element selector ', selector);
-            jlogs(f, ' wait for element url ', url);
-            // console.log(f, ' wait for element target ', load.getTarget(selector));
+        jlogs(f, ' wait for element selector ', selector);
+        jlogs(f, ' wait for element url ', url);
+        // console.log(f, ' wait for element target ', load.getTarget(selector));
 
-            // waitForSelector(url, selector, mapFunction, success, error)
-            // waitForSelector(url, selector, mapFunction, success, error);
+        // waitForSelector(url, selector, mapFunction, success, error)
+        // waitForSelector(url, selector, mapFunction, success, error);
 
-            var l = new Load(selector, success, error);
-            // var elem = document.querySelectorAll(selector)[0] || document.querySelectorAll(selector);
-            // if (elem) {
-            // callback executed when canvas was found
+        var l = new Load(selector, success, error);
+        // var elem = document.querySelectorAll(selector)[0] || document.querySelectorAll(selector);
+        // if (elem) {
+        // callback executed when canvas was found
 
-            // loadContentByUrls(jloads, object, mapFunction, success, error);
-            const funcName = getFunctionName(url, mapFunction, f);
-            jlogs(f, ' funcName ', funcName);
-            //jlogs(funcName, url, elem);
-            l[funcName](url);
+        // loadContentByUrls(jloads, object, mapFunction, success, error);
+        const funcName = getFunctionName(url, mapFunction, f);
+        jlogs(f, ' funcName ', funcName);
+        //jlogs(funcName, url, elem);
+        l[funcName](url);
         // }
 
 
     } else {
         var list = url;
-        if (isArray(list)) {
-            for (var i in list) {
-                var url = list[i];
+        jlogs(f, ' url4 ', url);
+        jlogs(f, ' list ', list);
 
-                jlogs(f, ' url3 ', url);
-                jlogs(f, ' list ', list);
-                getOne(load, url, selector, mapFunction, success, error);
-
-                // waitForSelector(url, selector, mapFunction, success, error);
-                //
-                // waitForSelector(url, selector, mapFunction, function () {
-                //     for (var i in url) {
-                //         var object = url[i];
-                //         jlogs(f, ' url4 i ', i);
-                //         jlogs(f, ' url4 object ', object);
-                //         for (var ii in object) {
-                //             jlogs(f, ' url5 object[ii], ii ', object[ii], ii);
-                //             getOne(load, object[ii], selector, mapFunction, success, error);
-                //         }
-                //     }//for
-                // }, error);
+        waitForSelector(url, selector, mapFunction, function () {
+            for (var i in url) {
+                var object = url[i];
+                jlogs(f, ' url4 i ', i);
+                jlogs(f, ' url4 object ', object);
+                for (var ii in object) {
+                    jlogs(f, ' url5 object[ii], ii ', object[ii], ii);
+                    getOne(load, object[ii], selector, mapFunction, success, error);
+                }
             }//for
-        }//if
+        }, error);
+
+        //
+        // if (isArray(list)) {
+        //     for (var i in list) {
+        //         var url = list[i];
+        //
+        //         jlogs(f, ' url4 ', url);
+        //         jlogs(f, ' list ', list);
+        //         getOne(load, url, selector, mapFunction, success, error);
+        //
+        //         // waitForSelector(url, selector, mapFunction, success, error);
+        //         //
+        //
+        //     }//for
+        // }//if
     }
     // error(elem);
 }

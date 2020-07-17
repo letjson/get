37e507aaf8bd9b3c806d7784203cb67b5e2ac850
tom <tom@sapletta.com>
2020-07-17 15:36:03 +0200

    New File solution for fast loading depends of files tree structure

--- jloads.js
+++ jloads.js
@@ -2974,18 +2974,24 @@ var jloads = function (selector) {
                     for (var selector in list) {
                         var l = new Load(selector, success, error); //.domain('localhost');
                         l.replaceOn();
+                        // console.log(f, '!!!4 l: ', l, self.mapFunction);
                         console.log(f, '!!!4 selector: ', selector, l, self.mapFunction);
 
-                        for (var id in list[selector]) {
-                            var url = list[selector][id];
-                            console.log(f, '!!!4 url: ', url);
-                            // getOne(self.jloads, url, selector, self.mapFunction, success, error)
-                            // loadContentByUrls(l, url, self.mapFunction, success, error);
-                            const funcName = getFunctionName(url, self.mapFunction, 'self.url');
-                            jlogs(f, '!!!4 funcName ', funcName);
-                            //jlogs(funcName, url, elem);
-                            l[funcName](url);
-                        }
+                        var url = list[selector];
+                        console.log(f, '!!!4 url: ', url);
+
+                        getOne(l, url, selector, self.mapFunction, success, error);
+                        //
+                        // for (var id in list[selector]) {
+                        //     var url = list[selector][id];
+                        //     console.log(f, '!!!4 url: ', url);
+                        //     // getOne(self.jloads, url, selector, self.mapFunction, success, error)
+                        //     // loadContentByUrls(l, url, self.mapFunction, success, error);
+                        //     const funcName = getFunctionName(url, self.mapFunction, 'self.url');
+                        //     jlogs(f, '!!!4 funcName ', funcName);
+                        //     //jlogs(funcName, url, elem);
+                        //     l[funcName](url);
+                        // }
                     }
                 }
 

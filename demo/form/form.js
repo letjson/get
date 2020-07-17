62eeb2d0ba91681f67aa16c1bcaed9943e0e85ad
tom <tom@sapletta.com>
2020-07-17 14:25:08 +0200

    New File solution for fast loading depends of files tree structure

--- demo/form/form.js
+++ demo/form/form.js
@@ -1,4 +1,3 @@
-// form.js
 /**
  *
  * @param text
@@ -11,6 +10,9 @@ function AddMessage(text) {
 
 var response = function (xhr) {
     console.log("xhr", xhr);
+    // console.log("b", b);
+    // console.log("c", c);
+    // alert("OK RESPONSE");
     AddMessage(xhr.status);
     AddMessage(xhr.statusText);
     AddMessage(xhr.response);
@@ -19,20 +21,7 @@ var response = function (xhr) {
 var success = function (data) {
     console.table('FORM success', data);
 };
+
 var error = function (data) {
     console.error('!FORM', data);
 }
-
-
-var form = new RestForm('form', response, error, success);
-
-form.cfg({
-    "target": "form",
-    "url": "//api.paas.info/index.php",
-    "method": "GET",
-    "event": "submit"
-});
-
-form.url((window.location.hostname === 'localhost') ? "//localhost:8000/index.php" : "//php.jloads.com/index.php");
-
-form.submit();
---
+++ demo/form/submit.js
@@ -0,0 +1,13 @@
+var form = new RestForm('form', response, error, success);
+
+form.cfg({
+    "target": "form",
+    // "url": "//php.jloads.com/index.php",
+    "url": "/deploy",
+    "method": "POST",
+    "event": "submit"
+});
+
+// form.url((window.location.hostname === 'localhost') ? "//localhost:8000/index.php" : "//php.jloads.com/index.php");
+
+form.submit();

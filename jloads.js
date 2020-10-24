b14c102d93be605c0bca7eb2b5114e1efb401b33
tom <tom@sapletta.com>
2020-10-24 13:00:04 +0200

    Config. not variable on the defining the class: new Load(cfg)

--- jloads.js
+++ jloads.js
@@ -1627,48 +1627,15 @@ if (typeof getOne !== 'function') getOne = function (load, url, selector, mapFun
     jlogs(f, ' url 2 ', url, typeof url, isString(url));
 
     if (isString(url)) {
-        // if (selector === 'head') {
-        //     loadContentByUrls(load, url, mapFunction, success, error);
-        //     success(load.getTarget(selector));
-        // } else if (selector === 'body') {
-        //     jlogs(f, ' wait for body selector ', selector);
-        //     jlogs(f, ' wait for body target ', load.getTarget(selector));
 
-        // var reload = document.querySelector(selector);
-        // reload.addEventListener("load", function (event) {
-        //
-        //     console.log(f, 'event::', event);
-        //
-        //     ReadyHtml(url, selector, mapFunction, success, error);
-        // });
-        // } else {
         jlogs(f, ' wait for element selector ', selector);
         jlogs(f, ' wait for element url ', url);
         // console.log(f, ' wait for element target ', load.getTarget(selector));
-
-        // waitForSelector(url, selector, mapFunction, success, error)
-        // waitForSelector(url, selector, mapFunction, success, error);
-
-        // var l = new Load({
-        //     target: selector,
-        //     success: success,
-        //     error: error,
-        //     replace: 0,
-        // });
-        //l.replaceOff();
-        // var elem = document.querySelectorAll(selector)[0] || document.querySelectorAll(selector);
-        // if (elem) {
-        // callback executed when canvas was found
-
-        // loadContentByUrls(jloads, object, mapFunction, success, error);
         var funcName = getFunctionName(url, mapFunction, f);
         jlogs(f, ' funcName ', funcName);
         //jlogs(funcName, url, elem);
         //l[funcName](url);
         load[funcName](url);
-        // }
-
-
 
     } else {
         var list = url;
@@ -1704,6 +1671,7 @@ if (typeof getOne !== 'function') getOne = function (load, url, selector, mapFun
             /// and load isObject list , {"page/text.html":["menu/radio.html"]},
 
             var afterLoaded = function () {
+
                 jlogs(f, 'afterLoaded ', selector, list[url]);
 
                 var l = new Load({
@@ -1713,20 +1681,6 @@ if (typeof getOne !== 'function') getOne = function (load, url, selector, mapFun
                     replace: 0,
                 });
                 getOne(l, list[url], selector, mapFunction);
-                /*
-                for (var i in list[url]) {
-                    var object = list[i];
-                    jlogs(f, 'isObject url6 i ', i);
-                    jlogs(f, 'isObject url6 object ', object);
-                    for (var ii in object) {
-                        jlogs(f, 'isObject url5 object[ii], ii ', object[ii], ii);
-
-                        load.replaceOn();
-                        getOne(load, object[ii], selector, mapFunction, success, error);
-                    }
-                }//for
-                */
-
             };
 
             var load = new Load({
